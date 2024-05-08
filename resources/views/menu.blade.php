505cc8b7f4ce14d27ce9e1460dc0338e91f8c13d
Fady Mondy <info@3x1.io>
2024-05-08 13:56:40 +0300

    update UI & Translations

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -6,19 +6,19 @@
         !data.asideMenuGroup['{{str_replace(" ", "_", $menu->name)}}']"
                 v-show="!data.makeMenuMin"
                 class="flex items-center justify-between w-full px-6">
-            <div class="flex items-center gap-4 text-gray-600 dark:text-gray-300">
+            <div class="flex items-center gap-4 text-zinc-600 dark:text-zinc-300">
                 <p class="flex-1 text-xs font-bold tracking-wider uppercase">
                     {{ $menu->name }}
                 </p>
             </div>
 
             <svg v-if="data.asideMenuGroup" v-show="data.asideMenuGroup['{{str_replace(" ", "_", $menu->name)}}']"
-                 class="w-3 h-3 text-gray-600 transition-all dark:text-gray-300" xmlns="http://www.w3.org/2000/svg"
+                 class="w-3 h-3 text-zinc-600 transition-all dark:text-zinc-300" xmlns="http://www.w3.org/2000/svg"
                  fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"></path>
             </svg>
             <svg v-if="data.asideMenuGroup" v-show="!data.asideMenuGroup['{{str_replace(" ", "_", $menu->name)}}']"
-                 class="w-3 h-3 text-gray-600 transition-all rotate-180 dark:text-gray-300"
+                 class="w-3 h-3 text-zinc-600 transition-all rotate-180 dark:text-zinc-300"
                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
                  aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"></path>
@@ -36,7 +36,7 @@
                                 @if($item->url === url()->current())
                                     bg-primary-500 text-white
                                 @else
-                                    hover:bg-gray-500/5 focus:bg-gray-500/5 dark:text-gray-300 dark:hover:bg-gray-700
+                                    hover:bg-zinc-500/5 focus:bg-zinc-500/5 dark:text-zinc-300 dark:hover:bg-zinc-700
                                 @endif
                               flex items-center justify-center w-full gap-3 px-3 py-2 font-medium transition rounded-lg">
 
@@ -69,7 +69,7 @@
                 @if(url()->current() === $menu->url)
                     bg-primary-500 text-white
                 @else
-                    hover:bg-gray-500/5 focus:bg-gray-500/5 dark:text-gray-300 dark:hover:bg-gray-700
+                    hover:bg-zinc-500/5 focus:bg-zinc-500/5 dark:text-zinc-300 dark:hover:bg-zinc-700
                 @endif
                 flex items-center justify-center gap-3 px-3 py-2 font-medium transition rounded-lg">
 
@@ -86,10 +86,10 @@
                 </li>
             </ul>
         </div>
-        <div class="my-6 ml-6 border-t rtl:ml-auto rtl:mr-6 dark:border-gray-700"></div>
+        <div class="my-6 ml-6 border-t rtl:ml-auto rtl:mr-6 dark:border-zinc-700"></div>
     @endif
     @if($counter !== count($menus)-1)
-        <div class="my-6 ml-6 border-t rtl:ml-auto rtl:mr-6 dark:border-gray-700"></div>
+        <div class="my-6 ml-6 border-t rtl:ml-auto rtl:mr-6 dark:border-zinc-700"></div>
     @endif
     @php $counter++; @endphp
 @endforeach
